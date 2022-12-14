659e21d4c2bb05685d963936d8643e7f08156486
RAHMA_BILAL <rahma.bilal15@gmail.com>
2022-12-14 14:51:46 +0000

    adding links to websites

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,9 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <h1>PORTFOLIO</h1>
+            <h6>DESIGNED BY
+              <br></br>
+              RAHMA BILAL</h6>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
@@ -20,10 +22,10 @@ export const Footer = () => {
               <a href="https://github.com/RahmaBilal">
                 <img src={github} alt="Icon" />
               </a>
-              <a href="https://twitter.com">
+              <a href="https://twitter.com/rxmacodes">
                 <img src={twitter} alt="Icon" />
               </a>
-              <a href="https://linkedin.com/in/rahmabilal">
+              <a href="https://linkedin.com/in/rahma-bilal">
                 <img src={linkedin} alt="Icon" />
               </a>
             </div>
